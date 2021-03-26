d8c3789d7013222bacc8fc3cd412a9c884ed728d
Matt Patterson <matt.patterson@hey.com>
2021-03-26 13:49:21 +1100

    Ensure dynamic colours are safelisted

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    options: {
+      safelist: [
+        'text-yellow-400',
+        'text-red-400',
+        'text-green-400',
+        'bg-green-50',
+        'bg-yellow-50',
+        'bg-red-50'
+      ]
+    }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
