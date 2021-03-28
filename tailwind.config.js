897216f9ab098823c5669a71225f8dc1dcf89c34
Matt Patterson <matt.patterson@hey.com>
2021-03-28 14:50:36 +1100

    Better safelist options

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,7 @@ module.exports = {
   purge: {
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     options: {
-      safelist: [
-        'text-yellow-400',
-        'text-red-400',
-        'text-green-400',
-        'bg-green-50',
-        'bg-yellow-50',
-        'bg-red-50'
-      ]
+      safelist: [/(text|bg)-(yellow|red|green|)-(.*)/]
     }
   },
   darkMode: false, // or 'media' or 'class'
