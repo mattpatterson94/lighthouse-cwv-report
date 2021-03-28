6b1e9e9b47f7fb1cdc17179874b77c17a0977193
Matt Patterson <matt.patterson@hey.com>
2021-03-29 10:19:48 +1100

    Safe list dynamic width and heights

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   purge: {
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     options: {
-      safelist: [/(text|bg)-(yellow|red|green|)-(.*)/]
+      safelist: [
+        /(text|bg)-(yellow|red|green|)-(.*)/,
+        /(w|h)-(5|7|10)/
+      ]
     }
   },
   darkMode: false, // or 'media' or 'class'
