27d3639b3e5b5111eff5f0be86e3b21a05a80de5
Matt Patterson <matt.patterson@hey.com>
2021-03-28 14:29:33 +1100

    Store API Key in localstorage

--- components/ApiKey/ApiKey.js
+++ components/ApiKey/ApiKey.js
@@ -1,5 +1,22 @@
+import { useEffect, useState } from 'react'
+
 export default function ApiKey({ updateFunc }) {
+  const [apiKey, setApiKey] = useState('')
+
+  useEffect(() => {
+    const localApiKey = localStorage.getItem('apiKey')
+
+    if(localApiKey) {
+      setApiKey(localApiKey)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('apiKey', apiKey)
+    updateFunc(apiKey)
+  }, [apiKey])
+
   return (
-    <input id="apiKey" name="apiKey" type="text" onChange={e => updateFunc(e.target.value)} required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Enter API Key" />
+    <input id="apiKey" name="apiKey" type="text" value={apiKey} onChange={e => setApiKey(e.target.value)} required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Enter API Key" />
   )
 }
